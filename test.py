fc11002e46955f7acc01f91a914e0951f9f75b70
roryclear <roryclear.rc@gmail.com>
2025-06-22 00:06:26 +0100

    faster?

--- test.py
+++ test.py
@@ -416,15 +416,12 @@ def fuse_score(cost_matrix, det_values):
 
 def linear_assignment(cost_matrix, thresh):
     if cost_matrix.size == 0:
-        return np.empty((0, 2), dtype=int), tuple(range(cost_matrix.shape[0])), tuple(range(cost_matrix.shape[1]))
-    matches, unmatched_a, unmatched_b = [], [], []
-    cost, x, y = lap.lapjv(cost_matrix, extend_cost=True, cost_limit=thresh)
-    for ix, mx in enumerate(x):
-        if mx >= 0:
-            matches.append([ix, mx])
-    unmatched_a = np.where(x < 0)[0]
+        return np.empty((0, 2), dtype=int), np.arange(cost_matrix.shape[0]), np.arange(cost_matrix.shape[1])
+    _, x, y = lap.lapjv(cost_matrix, extend_cost=True, cost_limit=thresh)
+    matched_mask = x >= 0
+    matches = np.column_stack((np.arange(len(x))[matched_mask],x[matched_mask]))
+    unmatched_a = np.where(~matched_mask)[0]
     unmatched_b = np.where(y < 0)[0]
-    matches = np.asarray(matches)
     return matches, unmatched_a, unmatched_b
 
 

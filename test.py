4ef6c9d5e95b843537fee868c196b824cdfed353
roryclear <roryclear.rc@gmail.com>
2025-06-23 13:56:00 +0100

    clean maybe

--- test.py
+++ test.py
@@ -345,6 +345,9 @@ class BYTETracker(object):
         self.tracked_stracks = [self.tracked_stracks[i] for i in keep_tracked] + [refind_stracks[i] for i in keep_refind]
         self.lost_stracks = [t for t in self.lost_stracks if t not in self.tracked_stracks]
         self.lost_stracks.extend([t for t in lost_stracks if t not in self.tracked_stracks])
+        self.lost_stracks_values = [tuple(t.values) for t in self.lost_stracks]
+        self.removed_stracks_values = set(tuple(t.values) for t in self.removed_stracks)
+        self.lost_stracks_values = [t for t in self.lost_stracks_values if t not in self.removed_stracks_values]
         self.lost_stracks = [t for t in self.lost_stracks if t not in self.removed_stracks]
         self.removed_stracks.extend(removed_stracks)
         
